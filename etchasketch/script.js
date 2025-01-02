b7d9b8c5d23e2612dae7155c90cabeab1322c99d
DomainNameExpansion <50421732+nocussing15@users.noreply.github.com>
2025-01-01 22:45:08 -0800

    Getting the grid just right
    Took what felt like a century, but my grid is now correct via flexbox. Now it's time to add
    the holographic portion and the drawing part.

--- etchasketch/script.js
+++ etchasketch/script.js
@@ -2,10 +2,28 @@
 const container = document.getElementById("grid-container"); 
 
 //loop to create the 256 squares 
+/*
 for (let i = 0 ; i<256; i++){
     let newSquare = document.createElement('div');
     newSquare.id ='square' + i;
     newSquare.className = 'squares';
     newSquare.textContent= 'test';
     container.appendChild(newSquare); 
+}
+*/
+for (let i = 0; i <16; i++) {
+    let newRow = document.createElement("div"); 
+    newRow.id='row' + i;
+    newRow.className = 'row';
+    container.appendChild(newRow);
+
+
+    for(let j = 0; j < 16; j++){
+        let newColumn = document.createElement("div");
+        newColumn.id = 'column' + j + 'of' + newRow.id; 
+        newColumn.className='column'; 
+        newColumn.textContent='test'; 
+        newRow.appendChild(newColumn);
+    }
+   
 }
